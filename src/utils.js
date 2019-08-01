e6d9d758d44371beaa857031102d4620a775ecbc
Michel Weststrate <mweststrate@gmail.com>
2019-08-01 15:06:51 +0200

    (temporarily) utils

--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,39 @@
 import { useState, useEffect, useCallback, useRef } from "react"
+import produce from "immer"
 
 export function useSocket(url, onMessage) {
-  const [socket, setSocket] = useState(null)
+  const socket = useRef()
   const msgHandler = useRef()
   msgHandler.current = onMessage
 
   useEffect(() => {
-    const socket = new WebSocket(url)
-    socket.onmessage = event => {
+    const createdSocket = new WebSocket(url)
+    createdSocket.onmessage = event => {
       const data = JSON.parse(event.data)
       msgHandler.current(data)
     }
-    setSocket(socket)
+    socket.current = createdSocket
+    console.log("created socket to " + url)
     return () => {
-      socket.close()
+      console.log("socket disconnected")
+      createdSocket.close()
     }
-  }, [url, msgHandler])
+  }, [url])
 
-  return useCallback(
-    data => {
-      socket.send(JSON.stringify(data))
-    },
-    [socket]
-  )
+  return useCallback(data => {
+    socket.current.send(JSON.stringify(data))
+  }, [])
+}
+
+export function produceWithPatches(arg1, arg2) {
+  if (typeof arg1 === "function") {
+    return (state, ...args) => produceWithPatches(state, draft => arg1(draft, ...args))
+  }
+  // non-curried form
+  let patches, inversePatches
+  const nextState = produce(arg1, arg2, (p, ip) => {
+    patches = p
+    inversePatches = ip
+  })
+  return [nextState, patches, inversePatches]
 }
