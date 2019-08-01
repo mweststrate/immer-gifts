b85d803dfdcfddde110bc16dc8dc54af63a54b96
Michel Weststrate <mweststrate@gmail.com>
2019-08-01 15:06:23 +0200

    Created socket server

--- server.js
+++ server.js
@@ -4,63 +4,47 @@ var wss = new WebSocketServer({ port: 5001 })
 /**
  * Connected clients
  */
-const connections = {
-  X: undefined,
-  O: undefined
-}
+const connections = []
 
 /**
- * Game(s) history as seen by server
+ * State as seen by server
  */
 const history = []
 
 wss.on("connection", function connection(ws) {
   /**
-   * Check if game isn't full
-   */
-  if (connections.X && connections.O) {
-    console.log("Refused connection, game full")
-    ws.send(
-      JSON.stringify({
-        type: "welcome",
-        player: null
-      })
-    )
-    return
-  }
-
-  /**
    * Assign player, save WS connection
    */
-  const player = !connections.X ? "X" : "O"
-  connections[player] = ws
-  console.log("Player " + player + " connected")
+  connections.push(ws)
+  console.log("New client connected")
 
   /**
    * Hanle new messages / closing
    */
   ws.on("message", function incoming(message) {
-    const recipient = player === "X" ? "O" : "X"
-    console.log(player, message)
+    console.log(message)
     history.push(...JSON.parse(message).patches)
-    if (connections[recipient]) connections[recipient].send(message)
-  })
-  ws.on("close", function() {
-    connections[player] = undefined
+    connections
+      .filter(client => client !== ws)
+      .forEach(client => {
+        client.send(message)
+      })
   })
 
   /**
-   * Send initial player assignment an game state
+   * Remove connection upon close
+   */
+  ws.on("close", function() {
+    const idx = connections.indexOf(ws)
+    if (idx !== -1) connections.splice(idx, 1)
+  })
+
+  /**
+   * Send initial state
    */
   ws.send(
     JSON.stringify({
-      type: "welcome",
-      player
-    })
-  )
-  ws.send(
-    JSON.stringify({
-      type: "patches",
+      type: "APPLY_PATCHES",
       patches: history
     })
   )
