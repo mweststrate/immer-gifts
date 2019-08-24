54055b1e0bc661a1625673db45587556bc7d119f
Michel Weststrate <mweststrate@gmail.com>
2019-08-24 22:04:18 +0200

    1.3 applied immer

--- src/gifts.js
+++ src/gifts.js
@@ -1,18 +1,24 @@
+import produce from "immer"
+
 export function addGift(state, id, description, image) {
-  state.gifts.push({
-    id,
-    description,
-    image,
-    reservedBy: undefined
+  return produce(state, draft => {
+    draft.gifts.push({
+      id,
+      description,
+      image,
+      reservedBy: undefined
+    })
   })
 }
 
 export function toggleReservation(state, giftId) {
-  const gift = state.gifts.find(gift => gift.id === giftId)
-  gift.reservedBy =
-    gift.reservedBy === undefined
-      ? state.currentUser.id
-      : gift.reservedBy === state.currentUser.id
-      ? undefined
-      : gift.reservedBy
+  return produce(state, draft => {
+    const gift = draft.gifts.find(gift => gift.id === giftId)
+    gift.reservedBy =
+      gift.reservedBy === undefined
+        ? state.currentUser.id
+        : gift.reservedBy === state.currentUser.id
+        ? undefined
+        : gift.reservedBy
+  })
 }
