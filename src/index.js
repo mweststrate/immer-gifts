4090c1164e8f5c8f12d7ef15dadf6fc204e1d788
Michel Weststrate <mweststrate@gmail.com>
2019-08-24 21:42:49 +0200

    1.0

--- src/index.js
+++ src/index.js
@@ -1,49 +1,6 @@
-import React, { useState } from "react"
+import React from "react"
 import ReactDOM from "react-dom"
-
-import { initialState, toggleReservation, addGift } from "./gifts"
 
 import "./misc/index.css"
 
-const Gift = ({ gift, users, currentUser, onReserve }) => (
-  <div className={`gift ${gift.reservedBy ? "reserved" : ""}`}>
-    <img src={gift.image} alt="gift" />
-    <div className="description">
-      <h2>{gift.description}</h2>
-    </div>
-    <div className="reservation">
-      {!gift.reservedBy || gift.reservedBy === currentUser.id ? (
-        <button onClick={() => onReserve(gift.id)}>{gift.reservedBy ? "Unreserve" : "Reserve"}</button>
-      ) : (
-        <span>{users[gift.reservedBy].name}</span>
-      )}
-    </div>
-  </div>
-)
-
-function GiftList() {
-  const state = initialState
-  const { users, gifts, currentUser } = state
-
-  const handleAdd = () => {}
-
-  const handleReserve = id => {}
-
-  return (
-    <div className="app">
-      <div className="header">
-        <h1>Hi, {currentUser.name}</h1>
-      </div>
-      <div className="actions">
-        <button onClick={handleAdd}>Add</button>
-      </div>
-      <div className="gifts">
-        {gifts.map(gift => (
-          <Gift key={gift.id} gift={gift} users={users} currentUser={currentUser} onReserve={handleReserve} />
-        ))}
-      </div>
-    </div>
-  )
-}
-
-ReactDOM.render(<GiftList />, document.getElementById("root"))
+ReactDOM.render(<h1>Hi</h1>, document.getElementById("root"))
