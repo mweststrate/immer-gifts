6e212079e5481d092897501d9ebdd9bbd1e83635
Michel Weststrate <mweststrate@gmail.com>
2019-08-24 21:54:32 +0200

    1.1 initial implementation

--- src/gifts.js
+++ src/gifts.js
@@ -1,7 +1,32 @@
-export function addGift(state, description, image) {
-  return state
+export function addGift(state, id, description, image) {
+  return {
+    ...state,
+    gifts: [
+      ...state.gifts,
+      {
+        id,
+        description,
+        image,
+        reservedBy: undefined
+      }
+    ]
+  }
 }
 
 export function toggleReservation(state, giftId) {
-  return state
+  return {
+    ...state,
+    gifts: state.gifts.map(gift => {
+      if (gift.id !== giftId) return gift
+      return {
+        ...gift,
+        reservedBy:
+          gift.reservedBy === undefined
+            ? state.currentUser.id
+            : gift.reservedBy === state.currentUser.id
+            ? undefined
+            : gift.reservedBy
+      }
+    })
+  }
 }
