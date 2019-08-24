477711de5531960d2d47587dee20fe9a22dfdbfb
Michel Weststrate <mweststrate@gmail.com>
2019-08-24 21:59:26 +0200

    1.2 naive implementation

--- src/gifts.js
+++ src/gifts.js
@@ -1,32 +1,18 @@
 export function addGift(state, id, description, image) {
-  return {
-    ...state,
-    gifts: [
-      ...state.gifts,
-      {
-        id,
-        description,
-        image,
-        reservedBy: undefined
-      }
-    ]
-  }
+  state.gifts.push({
+    id,
+    description,
+    image,
+    reservedBy: undefined
+  })
 }
 
 export function toggleReservation(state, giftId) {
-  return {
-    ...state,
-    gifts: state.gifts.map(gift => {
-      if (gift.id !== giftId) return gift
-      return {
-        ...gift,
-        reservedBy:
-          gift.reservedBy === undefined
-            ? state.currentUser.id
-            : gift.reservedBy === state.currentUser.id
-            ? undefined
-            : gift.reservedBy
-      }
-    })
-  }
+  const gift = state.gifts.find(gift => gift.id === giftId)
+  gift.reservedBy =
+    gift.reservedBy === undefined
+      ? state.currentUser.id
+      : gift.reservedBy === state.currentUser.id
+      ? undefined
+      : gift.reservedBy
 }
