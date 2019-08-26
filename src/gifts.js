e893fa69ce55f563e40bb79fb9f443df1d634ac9
Michel Weststrate <mweststrate@gmail.com>
2019-08-26 14:30:18 +0200

    8.0 back to separate logic

--- src/gifts.js
+++ src/gifts.js
@@ -3,6 +3,25 @@ import produce from "immer"
 import { allUsers, getCurrentUser } from "./misc/users"
 import defaultGifts from "./misc/gifts"
 
+export const addGift = produce((draft, id, description, image) => {
+  draft.gifts.push({
+    id,
+    description,
+    image,
+    reservedBy: undefined
+  })
+})
+
+export const toggleReservation = produce((draft, giftId) => {
+  const gift = draft.gifts.find(gift => gift.id === giftId)
+  gift.reservedBy =
+    gift.reservedBy === undefined
+      ? draft.currentUser.id
+      : gift.reservedBy === draft.currentUser.id
+      ? undefined
+      : gift.reservedBy
+})
+
 export function getInitialState() {
   return {
     users: allUsers,
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React, { useState, memo, useCallback } from "react"
 import ReactDOM from "react-dom"
 import uuidv4 from "uuid/v4"
-import { useImmer } from "use-immer"
 
-import { getInitialState } from "./gifts"
+import { getInitialState, addGift, toggleReservation } from "./gifts"
 
 import "./misc/index.css"
 
@@ -24,39 +23,21 @@ const Gift = memo(({ gift, users, currentUser, onReserve }) => (
 ))
 
 function GiftList() {
-  const [state, updateState] = useImmer(() => getInitialState())
+  const [state, setState] = useState(() => getInitialState())
   const { users, gifts, currentUser } = state
 
   const handleAdd = () => {
     const description = prompt("Gift to add")
     if (description)
-      updateState(
-        draft =>
-          void draft.gifts.push({
-            id: uuidv4(),
-            description,
-            image: `https://picsum.photos/id/${Math.round(Math.random() * 1000)}/200/200`,
-            reservedBy: undefined
-          })
-      )
+      setState(state => addGift(state, uuidv4(), description, "https://picsum.photos/200?q=" + Math.random()))
   }
 
   const handleReserve = useCallback(id => {
-    updateState(draft => {
-      const gift = draft.gifts.find(gift => gift.id === id)
-      gift.reservedBy =
-        gift.reservedBy === undefined
-          ? draft.currentUser.id
-          : gift.reservedBy === draft.currentUser.id
-          ? undefined
-          : gift.reservedBy
-    })
+    setState(state => toggleReservation(state, id))
   }, [])
 
   const handleReset = () => {
-    updateState(draft => {
-      return getInitialState()
-    })
+    setState(() => getInitialState())
   }
 
   return (
