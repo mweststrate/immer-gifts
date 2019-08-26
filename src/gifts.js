856711403b993b1284c05ce26c256f2073573664
Michel Weststrate <mweststrate@gmail.com>
2019-08-26 14:07:34 +0200

    Fix commit: addGift shouldn't have been there anymore

--- src/gifts.js
+++ src/gifts.js
@@ -3,14 +3,6 @@ import produce from "immer"
 import { allUsers, getCurrentUser } from "./misc/users"
 import defaultGifts from "./misc/gifts"
 
-export const addGift = produce((draft, id, description, image) => {
-  draft.gifts.push({
-    id,
-    description,
-    image,
-    reservedBy: undefined
-  })
-})
 export function getInitialState() {
   return {
     users: allUsers,
