2c9838562e20d9b5b4d8a77da99d4123fcd537a0
Michel Weststrate <mweststrate@gmail.com>
2019-08-26 12:37:24 +0200

    6.1 made all tests pass with reducer

--- src/gifts.spec.js
+++ src/gifts.spec.js
@@ -1,4 +1,4 @@
-import { toggleReservation, addGift, addBook, getBookDetails } from "./gifts"
+import { giftsReducer, getBookDetails } from "./gifts"
 
 const initialState = {
   users: [
@@ -32,7 +32,12 @@ const initialState = {
 }
 
 describe("Adding a gift", () => {
-  const nextState = addGift(initialState, "mug", "Coffee mug", "")
+  const nextState = giftsReducer(initialState, {
+    type: "ADD_GIFT",
+    id: "mug",
+    description: "Coffee mug",
+    image: ""
+  })
 
   test("added a gift to the collection", () => {
     expect(nextState.gifts.length).toBe(3)
@@ -44,7 +49,10 @@ describe("Adding a gift", () => {
 })
 
 describe("Reserving an unreserved gift", () => {
-  const nextState = toggleReservation(initialState, "egghead_subscription")
+  const nextState = giftsReducer(initialState, {
+    type: "TOGGLE_RESERVATION",
+    id: "egghead_subscription"
+  })
 
   test("correctly stores reservedBy", () => {
     expect(nextState.gifts[1].reservedBy).toBe(1) // Test user
@@ -66,7 +74,10 @@ describe("Reserving an unreserved gift", () => {
 })
 
 describe("Reserving an already reserved gift", () => {
-  const nextState = toggleReservation(initialState, "immer_license")
+  const nextState = giftsReducer(initialState, {
+    type: "TOGGLE_RESERVATION",
+    id: "immer_license"
+  })
 
   test("preserves stored reservedBy", () => {
     expect(nextState.gifts[0].reservedBy).toBe(2) // Someone else
@@ -86,14 +97,17 @@ describe("Reserving an already reserved gift", () => {
 describe("Can add books async", () => {
   test("Can add math book", async () => {
     const book = await getBookDetails("0201558025")
-    const nextState = addBook(initialState, book)
+    const nextState = giftsReducer(initialState, { type: "ADD_BOOK", book })
     expect(nextState.gifts[2].description).toBe("Concrete mathematics")
   })
 
   test("Can add two books in parallel", async () => {
     const promise1 = getBookDetails("0201558025")
     const promise2 = getBookDetails("9781598560169")
-    const nextState = addBook(addBook(initialState, await promise1), await promise2)
+    const nextState = giftsReducer(giftsReducer(initialState, { type: "ADD_BOOK", book: await promise1 }), {
+      type: "ADD_BOOK",
+      book: await promise2
+    })
     expect(nextState.gifts.length).toBe(4)
   })
 })
